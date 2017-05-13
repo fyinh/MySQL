bd92a7aa07d764f33612f7b495fb30c3b042486b
gaolao <543242960@qq.com>
2017-05-13 12:33:20 +0800

    step3

--- step3.sql
+++ step3.sql
@@ -23,7 +23,7 @@ where dept_name='computer';
 #3-6.修改每个系的年度预算，给该系的每个学生发2000元奖金。（修改每个系的年度预算为 原预算+该系人数*2000）。
 update department b,
 (select dept_name,cout(ID) as mun from student group by dept_name) tmp
-set b.budget = b.budget + tmp.num * 2000
+set b.budget = b.budget + tmp.num * 2000;
 
 #3-7.向department表中插入一条数据, dept_name属性的值为avg_budget, building为空, 年度预算为所有院系的年度预算平均值.
 
@@ -38,7 +38,7 @@ left join (
 	from exam
 ) as T1
 on T2.ID = T1.student_ID
-where T1.avg(grade) <= 70
+where T1.avg(grade) <= 70;
 
 #3-9.找出所有正在谈恋爱,但是学习成绩不佳(考试平均分低于75)的学生,强制将其情感状态改为单身.
 update students s,
